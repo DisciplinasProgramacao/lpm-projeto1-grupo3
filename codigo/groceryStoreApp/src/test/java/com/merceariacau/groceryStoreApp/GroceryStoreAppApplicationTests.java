6e562b120d7f059e8dc9f1b2d46ba9f752c682ff
Rafael Ferreira Fernandes <rafael680ferreira@gmail.com>
2023-03-01 17:56:55 -0300

    Initial commit

---
+++ codigo/groceryStoreApp/src/test/java/com/merceariacau/groceryStoreApp/GroceryStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merceariacau.groceryStoreApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
