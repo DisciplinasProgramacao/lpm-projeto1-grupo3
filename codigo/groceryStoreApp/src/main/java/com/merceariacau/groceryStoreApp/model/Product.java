5d2e04e5a41e82a99fc306a1cbc2ddd1b1d62f55
Rafael <rafael680ferreira@gmail.com>
2023-03-13 20:57:59 -0300

    creating controller

--- codigo/groceryStoreApp/src/main/java/com/merceariacau/groceryStoreApp/model/Product.java
+++ codigo/groceryStoreApp/src/main/java/com/merceariacau/groceryStoreApp/model/Product.java
@@ -1,23 +1,51 @@
 package com.merceariacau.groceryStoreApp.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name="Employee", uniqueConstraints={@UniqueConstraint(columnNames={"ID"})})
 public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID", nullable=false, unique=true, length=11)
+	private Integer id;
+	@Column(name="DESCRIPTION", length=20, nullable=true)
 	private String description;
+	@Column(name="SALEPRICE", length=20, nullable=true)
 	private Double salePrice;
+	@Column(name="COSTPRICE", length=20, nullable=true)
 	private Double costPrice;
+	@Column(name="NAME", length=20, nullable=true)
 	private Double profitMargin;
+	@Column(name="NAME", length=20, nullable=true)
 	private Double tax;
+	@Column(name="NAME", length=20, nullable=true)
+	private Integer quantity;
 	
-	public Product(String description, Double costPrice, Integer percentageProfitMargin) {
-		this.description = description;
+	public Product(String description, Double costPrice, Integer quantity, Integer percentageProfitMargin) {
+		this.description = validateDescription(description);
 		this.costPrice = costPrice;
+		this.quantity = quantity;
 		this.profitMargin = calculateProfitMargin(percentageProfitMargin, costPrice);
 		this.tax = calculateTaxValue(costPrice, profitMargin);
 		this.salePrice = calculateSalePrice(costPrice, profitMargin, tax);
+	}
+	private String validateDescription(String description) {
+		if(description.length() < 3) {
+			System.out.println("A descrição do produto deve conter pelo menos 3 caracteres.");
+			return null;
+		}
+		return description;
 	}
 	
 	private Double calculateProfitMargin(Integer percentageProfitMargin, Double costPrice) {
